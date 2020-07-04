b8ac000f909b775df1c2cf52be6e12b40c30bc8c
wasauskyok <daniechoque159@gmail.com>
2020-07-04 06:18:21 -0500

    udpate package

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import  inquirir from 'inquirer'
-import {exec} from 'promisify-child-process'
-const data_babel_rc=`{
+import inquirir from "inquirer";
+import { exec } from "promisify-child-process";
+const data_babel_rc = `{
     "presets": [
         "@babel/preset-env"
     ]
-}` 
-const data_git_ignore=`.env
+}`;
+const data_git_ignore = `.env
 .git
 node_modules
-`
+`;
 // const carpeta_src=`
 //     mkdir src && cd src &&
 //     touch index.js && echo 'require("@babel/polyfill");
@@ -17,15 +17,15 @@ node_modules
 //         extensions: [".es6", ".es", ".jsx", ".js", ".mjs"],
 //         ignore:[/node_modules/]
 //     });
-    
+
 //     require("./handler/"+this.process.handler);' > index.js &&
-//     mkdir datalayer functions handler 
+//     mkdir datalayer functions handler
 // `
-const package_json=(parameter)=>`
+const package_json = (parameter) => `
 {
     "name": "${parameter.package_name}",
     "version": "${parameter.version}",
-    "description": "${"version" in parameter?parameter.description:""}",
+    "description": "${"version" in parameter ? parameter.description : ""}",
     "main": "src/index.js",
     "scripts": {
       "test": "echo testing && exit 1",
@@ -35,12 +35,16 @@ const package_json=(parameter)=>`
     },
     "keywords": [],
     ${
-        parameter.git_repository !=''? `
+      parameter.git_repository != ""
+        ? `
         "repository": {
             "type": "git",
-            "url": "${"git_repository"in parameter?parameter.git_repository:"\t"}"
+            "url": "${
+              "git_repository" in parameter ? parameter.git_repository : "\t"
+            }"
           },     
-        `:""
+        `
+        : ""
     }
    
     
@@ -59,8 +63,8 @@ const package_json=(parameter)=>`
       "nodemon": "^2.0.4"
     }
   }
-  `
-  console.log(`
+  `;
+console.log(`
 
   ████████╗███████╗███╗   ███╗██████╗ ██╗      █████╗ ████████╗███████╗      ███████╗███████╗ ██████╗       ███╗   ██╗ ██████╗ ██████╗ ███████╗
   ╚══██╔══╝██╔════╝████╗ ████║██╔══██╗██║     ██╔══██╗╚══██╔══╝██╔════╝      ██╔════╝██╔════╝██╔════╝       ████╗  ██║██╔═══██╗██╔══██╗██╔════╝
@@ -69,69 +73,72 @@ const package_json=(parameter)=>`
      ██║   ███████╗██║ ╚═╝ ██║██║     ███████╗██║  ██║   ██║   ███████╗      ███████╗███████║╚██████╔╝      ██║ ╚████║╚██████╔╝██████╔╝███████╗
      ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝      ╚══════╝╚══════╝ ╚═════╝       ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝
                                                                                                                                                                                                                                                                                                                                      
-  `)
-  console.log(`
+  `);
+console.log(`
 ┌┐ ┬ ┬  ┌─┐┬ ┬┌─┐┌─┐ ┬ ┬┌─┐┌─┐┌─┐┬ ┬┬─┐┬ ┬┌─┐
 ├┴┐└┬┘  │  ├─┤│ ││─┼┐│ │├┤ └─┐├─┤│ │├┬┘│ │└─┐
 └─┘ ┴   └─┘┴ ┴└─┘└─┘└└─┘└─┘└─┘┴ ┴└─┘┴└─└─┘└─┘
-  `)
-  //let nom_carpeta=""
-inquirir.prompt([
+  `);
+console.log("**********************");
+//let nom_carpeta=""
+inquirir
+  .prompt([
     {
-        type:'input',
-        name:"working_dir",
-        message:"¿Nombre de la carpeta a crear (Working directory)?: ",
-        default:"Carpeta_"+new Date().toLocaleString().replace(/[\s:\/]/g,"-")
-       // default:"Carpeta_"+new Date().toLocaleString().replace(/\//g,"-")
-     },
+      type: "input",
+      name: "working_dir",
+      message: "¿Nombre de la carpeta a crear (Working directory)?: ",
+      default:
+        "Carpeta_" + new Date().toLocaleString().replace(/[\s:\/]/g, "-"),
+      // default:"Carpeta_"+new Date().toLocaleString().replace(/\//g,"-")
+    },
     //  {
     //      type:'list',
     //      name:"npm_init",
     //      message:"¿Deseas el package.json automatico o manual ? (npm init --yes) :?",
-    //      choices:["Automatico","Manual"]       
+    //      choices:["Automatico","Manual"]
     // },
 
     {
-        type:'input',
-        name:"package_name",
-        message:"Configurando package.json :\npackage name :",
-        default:"default(working_dir)"
-        //when:(entrada) => {nom_carpeta=entrada.working_dir; return true}
+      type: "input",
+      name: "package_name",
+      message: "Configurando package.json :\npackage name :",
+      default: "default(working_dir)",
+      //when:(entrada) => {nom_carpeta=entrada.working_dir; return true}
     },
     {
-        type:'input',
-        name:"version",
-        message:"version : ",
-        default:'v1.0.0'
-        //when:(entrada) => entrada.npm_init == 'Manual'?true:false
+      type: "input",
+      name: "version",
+      message: "version : ",
+      default: "v1.0.0",
+      //when:(entrada) => entrada.npm_init == 'Manual'?true:false
     },
     {
-        type:'input',
-        name:"description",
-        message:"description : ",
-        default:''
-        //when:(entrada) => entrada.npm_init == 'Manual'?true:false
+      type: "input",
+      name: "description",
+      message: "description : ",
+      default: "",
+      //when:(entrada) => entrada.npm_init == 'Manual'?true:false
     },
     {
-        type:'input',
-        name:"git_repository",
-        message:"git repository : ",
-        default:''
-        //when:(entrada) => entrada.npm_init == 'Manual'?true:false
+      type: "input",
+      name: "git_repository",
+      message: "git repository : ",
+      default: "",
+      //when:(entrada) => entrada.npm_init == 'Manual'?true:false
     },
     {
-        type:'input',
-        name:"author",
-        message:"author : ",
-        default:''
-       //when:(entrada) => entrada.npm_init == 'Manual'?true:false
+      type: "input",
+      name: "author",
+      message: "author : ",
+      default: "",
+      //when:(entrada) => entrada.npm_init == 'Manual'?true:false
     },
     {
-        type:'input',
-        name:"license",
-        message:"Licencia : ",
-        default:'MIT'
-        //when:(entrada) => entrada.npm_init == 'Manual'?true:false
+      type: "input",
+      name: "license",
+      message: "Licencia : ",
+      default: "MIT",
+      //when:(entrada) => entrada.npm_init == 'Manual'?true:false
     },
     // {
     //     type:'list',
@@ -155,72 +162,102 @@ inquirir.prompt([
     },
     */
     {
-        type:'checkbox',
-        name:"archivos_primarios",
-        message:"Marca que deseas generar automaticamente : ",
-        choices:[
-            ".babelrc",
-            ".env",
-            "Dockerfile",
-            ".gitignore",
-            "Docker-compose.yml",
-            //"eslint",
-            "readme.md"      
-        ]
+      type: "checkbox",
+      name: "archivos_primarios",
+      message: "Marca que deseas generar automaticamente : ",
+      choices: [
+        ".babelrc",
+        ".env",
+        "Dockerfile",
+        ".gitignore",
+        "Docker-compose.yml",
+        //"eslint",
+        "readme.md",
+      ],
     },
     {
-        type:'list',
-        name:'npm_install',
-        message:'¿Deseas que se instale automaticamente las dependencias requeridas?',
-        choices:[
-            "Si",
-            "No"
-        ]
-    },{
-        type:'list',
-        name:'open_proyect',
-        message:'Cual es el comando para importar tu proyecto en tu editor de codigo : ?',
-        choices:["code",'mate','subl','atom','Ninguna de las anteriores']
-    }
-]).then(async data=>{
-    const  {package_name:proccess_package_name,author,license,git_repository,version,description} = data
-    const package_name=proccess_package_name == "default(working_dir)"?data.working_dir:proccess_package_name
-    const {stdout} = await exec(`mkdir ${data.working_dir}`)
+      type: "list",
+      name: "npm_install",
+      message:
+        "¿Deseas que se instale automaticamente las dependencias requeridas?",
+      choices: ["Si", "No"],
+    },
+    {
+      type: "list",
+      name: "open_proyect",
+      message:
+        "Cual es el comando para importar tu proyecto en tu editor de codigo : ?",
+      choices: ["code", "mate", "subl", "atom", "Ninguna de las anteriores"],
+    },
+  ])
+  .then(async (data) => {
+    const {
+      package_name: proccess_package_name,
+      author,
+      license,
+      git_repository,
+      version,
+      description,
+    } = data;
+    const package_name =
+      proccess_package_name == "default(working_dir)"
+        ? data.working_dir
+        : proccess_package_name;
+    const { stdout } = await exec(`mkdir ${data.working_dir}`);
     //data["npm_init"] == 'Automatico'?
-    await exec(`git init`,{cwd:data.working_dir})
-    await exec(`touch package.json && echo '${package_json({package_name,author,license,git_repository,author,version,description})}'>package.json`,{cwd:data.working_dir})
+    await exec(`git init`, { cwd: data.working_dir });
+    await exec(
+      `touch package.json && echo '${package_json({
+        package_name,
+        author,
+        license,
+        git_repository,
+        author,
+        version,
+        description,
+      })}'>package.json`,
+      { cwd: data.working_dir }
+    );
     //:await exec(`touch package.json && echo '${package_json(destructuring_data)}'`,{cwd:data.working_dir})
-    for(let item  of data.archivos_primarios){
-        if(item == '.babelrc')
-        {
-            await exec (`touch ${item} && echo '${data_babel_rc}'> ${item}`,{cwd:data.working_dir})
-        }
-        if(item == '.gitignore')
-        {
-            await exec (`touch ${item} && echo '${data_git_ignore}'> ${item}`,{cwd:data.working_dir})
-        }
-        if(item == '.env')
-        {
-            await exec (`touch ${item} && echo 'handler=express'>${item}`,{cwd:data.working_dir})
-        }
-        await exec (`touch ${item}`,{cwd:data.working_dir})
+    for (let item of data.archivos_primarios) {
+      if (item == ".babelrc") {
+        await exec(`touch ${item} && echo '${data_babel_rc}'> ${item}`, {
+          cwd: data.working_dir,
+        });
+      }
+      if (item == ".gitignore") {
+        await exec(`touch ${item} && echo '${data_git_ignore}'> ${item}`, {
+          cwd: data.working_dir,
+        });
+      }
+      if (item == ".env") {
+        await exec(`touch ${item} && echo 'handler=express'>${item}`, {
+          cwd: data.working_dir,
+        });
+      }
+      await exec(`touch ${item}`, { cwd: data.working_dir });
     }
-    await exec('git clone https://github.com/WasauskyOK/src.git',{cwd:data.working_dir})
-    if(data.npm_install == 'Si'){
-        console.log('Instalando dependencias necesarias ...')
-        await exec('npm install',{cwd:data.working_dir})
-        console.log('Dependencias instaladas ... \n')
+    await exec("git clone https://github.com/WasauskyOK/src.git", {
+      cwd: data.working_dir,
+    });
+    if (data.npm_install == "Si") {
+      console.log("Instalando dependencias necesarias ...");
+      await exec("npm install", { cwd: data.working_dir });
+      console.log("Dependencias instaladas ... \n");
     }
-    console.log("Ejecutando en desarrollo (npm run dev) en una nueva terminal :D")
-    await exec(`gnome-terminal -- bash -c 'npm run dev; exec zsh || exec bash'`,{cwd:data.working_dir}) 
-    data.open_proyect=='Ninguna de las anteriores'?
-    console.log('Abre con tu editor favorito :D'):
-    await exec(`${data.open_proyect} .`,{cwd:data.working_dir})
-    console.log("Adios :D")
-   
-   
+    console.log(
+      "Ejecutando en desarrollo (npm run dev) en una nueva terminal :D"
+    );
+    await exec(
+      `gnome-terminal -- bash -c 'npm run dev; exec zsh || exec bash'`,
+      { cwd: data.working_dir }
+    );
+    data.open_proyect == "Ninguna de las anteriores"
+      ? console.log("Abre con tu editor favorito :D")
+      : await exec(`${data.open_proyect} .`, { cwd: data.working_dir });
+    console.log("Adios :D");
+
     //await exec(carpeta_src,{cwd:data.working_dir})
     //await Promise.all(data.archivos_primarios.map(async (item)=>await exec (`touch ${item}`,{cwd:data.working_dir})))
-    
-}
-    ).catch(error=>console.log(error))
+  })
+  .catch((error) => console.log(error));
